43a7570dd68497ba8d9535ee935ae93a019f2e83
Sergii Tsegelnyk <dr3am3r.ua@gmail.com>
2016-11-01 18:59:28 +0200

    search by title basic test

--- test/search.js
+++ test/search.js
@@ -1,7 +1,18 @@
-const assert = require('assert');
+const supertest = require('supertest');
+const request = supertest('http://localhost:3000');
 
-describe('Check', function() {
-    it('should be ok', function() {
-        assert(true, 'true is true');
+describe('Search', function() {
+    describe('GET /search', function() {
+        it('Should return 200 with json with movies found', function(done) {
+            request
+                .get('/search?title=matrix')
+                .set('Accept', 'application/json')
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) return done(err);
+                    console.log('@data ', res);
+                    done();
+                });
+        });
     });
 })
